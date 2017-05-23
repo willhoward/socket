b05e9ef0238d1dc1123dd180515b313d53467581
Will Howard <willhowardgb@gmail.com>
2017-05-23 22:52:11 +0100

    Initial commit, project created.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+const config = {
+  apiKey: 'AIzaSyD3mRdsyHcPf0FbhkbJWf0ZkjMPRG9Ttmc',
+  authDomain: 'socket-65ebb.firebaseapp.com',
+  databaseURL: 'https://socket-65ebb.firebaseio.com',
+  projectId: 'socket-65ebb',
+  storageBucket: 'socket-65ebb.appspot.com',
+  messagingSenderId: '473787475654',
+};
+
+firebase.initializeApp(config);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
