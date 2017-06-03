e51110abf881188b1c8882b0cbf8494cf840c152
willhowardgb <willhowardgb@gmail.com>
2017-06-03 23:26:57 +0100

    Returning all chats in list.

--- src/components/chats.js
+++ src/components/chats.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from 'firebase';
 
-const Chats = () => (
-  <ul className="chats" />
-);
+class Chats extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      chats: [],
+    };
+  }
+
+  componentWillMount() {
+    firebase.database().ref('/chats').once('value').then(snap => {
+      const chats = [];
+      Object.keys(snap.val()).forEach(i => {
+        const chat = snap.val()[i];
+        chats.push(chat);
+      });
+      this.setState({ chats });
+    });
+  }
+
+  render() {
+    const { chats } = this.state;
+    return (
+      <ul className="chats">
+        { chats.map(chat => (
+          <li key={`${chat.members[0]}_${chat.members[1]}`}>
+            <p className="black">{`${chat.members[0]}_${chat.members[1]}`}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default Chats;
