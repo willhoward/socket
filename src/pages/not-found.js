07b5d2c447ca558fd751322dd3c8ed5730fcd5e3
willhowardgb <willhowardgb@gmail.com>
2017-06-07 17:45:32 +0100

    Updated not found page.

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NotFound = () => (
-  <p>Nothing found.</p>
+  <p>Nothing found. Care to head <a className="white" href="/">home</a>?</p>
 );
 
 export default NotFound;
